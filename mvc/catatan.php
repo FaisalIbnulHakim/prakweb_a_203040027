ef4f33ad8a3733baeb92499dc312563b0239aba4
FaisalIbnulHakim <203040027@mail.unpas.ac.id>
2022-10-05 19:07:10 +0700

    menyelesaikan video 2 (persiapan)

---
+++ mvc/catatan.php
@@ -0,0 +1,26 @@
+<?php  
+/*
+  MVC adalah 
+  Pola arsitektur pada perancangan lunak berorientasi objek
+  Memisahkan antara tampilan, data, dan proses
+
+  Kenapa mvc
+  - Organisasi dan struktur kode
+  - pemishan logic dan tampilan
+  - perawatan kode
+  - implementasi konsep oop yang sudah di pelajari
+  - digunakan oleh banyak framework
+
+  Model
+  - representasi pengetahuan
+  - mengelola data
+  - logika bisnis
+  Controller
+  - perantara antara model
+  - menangani pemrosesan pada aplikasi
+  - menangani aksi dari user
+  View
+  - output, apa yang dilihat oleh user
+  - representasi visual dari model
+  - lapisan representasi
+*/
