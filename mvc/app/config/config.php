3925869ce461d6de2f0ab6643cdb5776c524c9b3
FaisalIbnulHakim <203040027@mail.unpas.ac.id>
2022-10-06 17:28:14 +0700

    menyelesaikan video 8 (database wrapper)

---
+++ mvc/app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+define('BASEURL', 'http://localhost/prakweb_a_203040027/mvc/public');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mvc');
--- mvc/app/core/Constants.php
+++
@@ -1,2 +0,0 @@
-<?php
-define('BASEURL', 'http://localhost/prakweb_a_203040027/mvc/public');
